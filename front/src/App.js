f728c784f6ecfe9852b350f74963d692feb02873
Michaël Perrin <contact@michaelperrin.fr>
2018-09-19 15:31:22 -0500

    Use constant component

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UpdateProfilePictureForm from './UpdateProfilePictureForm';
 import APOLLO_CLIENT from './constants/apolloClient';
 import { ApolloProvider } from 'react-apollo';
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={APOLLO_CLIENT}>
-        <div className="App">
-          <UpdateProfilePictureForm />
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
+const App = () => (
+  <ApolloProvider client={APOLLO_CLIENT}>
+    <div className="App">
+      <UpdateProfilePictureForm />
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
